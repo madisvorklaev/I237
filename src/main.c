352b989c31336f944d5525d16c5643be996cbf6e
Madis <madisvorklaev@gmail.com>
2017-11-01 20:55:51 +0200

    lab2 done!

--- src/main.c
+++ src/main.c
@@ -1,20 +1,23 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define BLINK_DELAY_MS 1000
+#define BLINK_DELAY_MS 500
+
+void blink (const char pin) /* Function for toggleing PORTA pins */
+{
+    DDRA = 0x15; /* Set port A pins 0,2,4 (00010101=15h) for outputs */
+    PORTA |= _BV(pin); /*scan whole PORTA and shift 1 zero positions to the left (_BV=(1<<0)) */
+    _delay_ms(BLINK_DELAY_MS);
+    PORTA &= ~_BV(pin); /* ~=bitwise NOT, toggle bit value */
+    _delay_ms(BLINK_DELAY_MS);
+}
 
 void main (void)
 {
-    /* Set port B pin 7 for output for Arduino Mega yellow LED */
-    DDRB |= _BV(DDB7);
-
     while (1) {
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED on */
-        PORTB |= _BV(PORTB7);
-        _delay_ms(BLINK_DELAY_MS);
-        /* Set port B pin 7 high to turn Arduino Mega yellow LED off */
-        PORTB &= ~_BV(PORTB7);
-        _delay_ms(BLINK_DELAY_MS);
+        blink(PA2);
+        blink(PA2);
+        blink(PA2);
     }
 }
 
--- tooling/format-code.sh
+++ tooling/format-code.sh
@@ -4,20 +4,20 @@
 # See http://astyle.sourceforge.net/astyle.html for syntax and defaults.
 
 MINPARAMS=1
-ORIG_SUFFIX=orig
+          ORIG_SUFFIX=orig
 
 # Exit error when not enough arguments.
-if [ $# -lt "$MINPARAMS" ]
-then
-    echo "This script needs C source files passed as arguments"
-    echo "USAGE: format-code.sh src/main.c src/somecode.c ..."
-    exit 1
-fi
+                      if [ $# -lt "$MINPARAMS" ]
+                          then
+                          echo "This script needs C source files passed as arguments"
+                          echo "USAGE: format-code.sh src/main.c src/somecode.c ..."
+                          exit 1
+                          fi
 
 # For all files provided as arguments.
-for FILE in "$@"
-do
-    RESULT="$(astyle    --style=1tbs \
+                          for FILE in "$@"
+                          do
+                              RESULT="$(astyle    --style=1tbs \
                         --indent-col1-comments \
                         --break-blocks \
                         --pad-oper \
@@ -32,19 +32,19 @@ do
                         --lineend=linux \
                         $FILE)"
 
-    # If file unchanged print unchanged result message.
-    if [[ "$RESULT" = Unchanged* ]]
-    then
-        echo "$RESULT"
-    fi
+# If file unchanged print unchanged result message.
+                                     if [[ "$RESULT" = Unchanged* ]]
+                                             then
+                                             echo "$RESULT"
+                                             fi
 
-    # If file formatted print result and renamed original file name.
-    if [[ "$RESULT" = Formatted* ]]
-    then
-        echo "$RESULT"
-        echo "Original code was preserved in file $FILE.$ORIG_SUFFIX"
-    fi
-done
+# If file formatted print result and renamed original file name.
+                                             if [[ "$RESULT" = Formatted* ]]
+                                                 then
+                                                 echo "$RESULT"
+                                                 echo "Original code was preserved in file $FILE.$ORIG_SUFFIX"
+                                                 fi
+                                                 done
 
-exit 0
+                                                 exit 0
 
