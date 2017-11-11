239c9e74a0952b2649dea94c83ecc5adfcc1a4e5
Madis <madisvorklaev@gmail.com>
2017-11-12 00:08:39 +0200

    lab3 done!

--- src/main.c
+++ src/main.c
@@ -1,23 +1,61 @@
+#include <stdio.h>
+#define __ASSERT_USE_STDERR
+#include <assert.h> /* The C library macro void assert(int expression) allows diagnostic information to be written to the standard error file. */
 #include <avr/io.h>
 #include <util/delay.h>
+#include "uart.h"
 
-#define BLINK_DELAY_MS 500
+#define BLINK_DELAY_MS 100
 
 void blink (const char pin) /* Function for toggleing PORTA pins */
 {
     DDRA = 0x15; /* Set port A pins 0,2,4 (00010101=15h) for outputs */
-    PORTA |= _BV(pin); /*scan whole PORTA and shift 1 zero positions to the left (_BV=(1<<0)) */
+    PORTA |= _BV(
+                 pin); /* scan whole PORTA and shift 1 zero positions to the left (_BV=(1<<0)) */
     _delay_ms(BLINK_DELAY_MS);
     PORTA &= ~_BV(pin); /* ~=bitwise NOT, toggle bit value */
     _delay_ms(BLINK_DELAY_MS);
 }
 
+/* Init error console as stderr in UART1 and print user code info */
+static inline void init_errcon(void)
+{
+    simple_uart1_init();
+    stderr = &simple_uart1_out;
+    fprintf(stderr, "Version: %s built on: %s %s\n",
+            FW_VERSION, __DATE__, __TIME__);
+    fprintf(stderr, "avr-libc version: %s avr-gcc version: %s\n",
+            __AVR_LIBC_VERSION_STRING__, __VERSION__);
+}
+
 void main (void)
 {
+    init_errcon();
+    /* Test assert - REMOVE IN FUTURE LABS */
+    char *array;
+    uint32_t i = 1;
+    extern int __heap_start,
+           *__brkval; /* http://users.ece.utexas.edu/~valvano/arm/heap.c */
+    int v;
+    array = malloc( i * sizeof(char));
+    assert(array);
+
+    /* End test assert */
     while (1) {
+        blink(PA0);
         blink(PA2);
-        blink(PA2);
-        blink(PA2);
+        blink(PA4);
+        /* Test assert - REMOVE IN FUTURE LABS */
+        /*
+         * Increase memory allocated for array by 100 chars
+         * until we have eaten it all and print space between stack and heap.
+         * That is how assert works in run-time.
+         */
+        array = realloc( array, (i++ * 100) * sizeof(char));
+        fprintf(stderr, "%d\n",
+                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
+        assert(array);
+        /* End test assert */
     }
 }
 
--- src/uart.c
+++ src/uart.c
@@ -1,40 +1,70 @@
 #include <avr/io.h>
 #include <stdio.h>
 
-#ifndef F_CPU
-#define F_CPU 16000000UL
-#endif
-
 #ifndef BAUD
 #define BAUD 9600
 #endif
 #include <util/setbaud.h>
 
 /* http://www.cs.mun.ca/~rod/Winter2007/4723/notes/serial/serial.html */
+/* http://www.atmel.com/Images/Atmel-2549-8-bit-AVR-Microcontroller-ATmega640-1280-1281-2560-2561_datasheet.pdf */
 
-void uart_init(void) {
-    UBRR0H = UBRRH_VALUE;
-    UBRR0L = UBRRL_VALUE;
-    
+void simple_uart0_init(void) /* initialize UART0 and set baud rate */
+{
+    UBRR0H = UBRRH_VALUE; /* Uart Baud Rate Register is used to set the clock for the UART shift registers */
+    UBRR0L = UBRRL_VALUE; /* The UBRRnH contains the four most significant bits, and the UBRRnL contains the eight least significant bits of the USART baud rate. */
 #if USE_2X
-    UCSR0A |= _BV(U2X0);
+    UCSR0A |= _BV(
+                  U2X0); /* The transfer rate can be doubled by setting the U2Xn bit in UCSRnA register */
 #else
     UCSR0A &= ~(_BV(U2X0));
 #endif
-
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */ 
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */    
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* UCSZ=UsartCharacterSiZe, 8-bit data */
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
 }
 
-void uart_putchar(char c, FILE *stream) {
-    if (c == '\n') {
-        uart_putchar('\r', stream);
+void simple_uart1_init()
+{
+    UBRR1H = UBRRH_VALUE;
+    UBRR1L = UBRRL_VALUE;
+#if USE_2X
+    UCSR1A |= _BV(U2X1);
+#else
+    UCSR1A &= ~(_BV(U2X1));
+#endif
+    UCSR1C = _BV(UCSZ11) | _BV(UCSZ10);
+    UCSR1B = _BV(TXEN1);   /* Enable TX only (STDERR) */
+}
+
+int simple_uart0_putchar(char c, FILE *stream) /* output to UART0 */
+{
+    (void) stream;
+
+    if (c == '\n') { /* newline */
+        simple_uart0_putchar('\r', stream); /* carriage return */
     }
-    loop_until_bit_is_set(UCSR0A, UDRE0);
-    UDR0 = c;
+
+    loop_until_bit_is_set(UCSR0A, UDRE0); /* UDRE=Usart Data Register Empty */
+    UDR0 = c; /* USART data register */
+    return 0;
 }
 
-char uart_getchar(FILE *stream) {
-    loop_until_bit_is_set(UCSR0A, RXC0);
-    return UDR0;
+int simple_uart1_putchar(char c, FILE *stream) /* output to UART1 */
+{
+    (void) stream;
+
+    if (c == '\n') {
+        simple_uart1_putchar('\r', stream);
+    }
+
+    loop_until_bit_is_set(UCSR1A, UDRE1);
+    UDR1 = c; /* USART data register */
+    return 0;
+}
+
+int simple_uart0_getchar(FILE *stream) /* input from UART0 */
+{
+    (void) stream;
+    loop_until_bit_is_set(UCSR0A, RXC0); /* RXCn=RX Complete handler */
+    return UDR0; /* TX/RX buffer */
 }
