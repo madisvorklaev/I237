a013159e49f42e3ca401c98e8f352de48364a84c
Madis <madisvorklaev@gmail.com>
2017-11-28 22:36:11 +0200

    Version print in hmi_msg

--- src/hmi_msg.h
+++ src/hmi_msg.h
@@ -1,2 +1,15 @@
-char *name = "Madis Vorklaev";
+#include <avr/pgmspace.h>
+#ifndef HMI_MSG /* prevent the header file to be included more than once, see "wrapper #ifndef" */
+#define HMI_MSG
+
+#define USERNAME "Madis Vorklaev"
+#define VER_FW "Version: %s built on: %s %s\n"
+#define VER_LIBC "avr-libc version: %s"
+#define VER_GCC  "avr-gcc version: %s\n"
+
+//char *name = "Madis vVorklaev";
+//char *VER_FW = ("Version: FW_VERSION built on: __DATE__ __TIME__\n");
 const char *lookup[10] = {"Zero", "One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+
+
+#endif /* HMI_MSG */
--- src/main.c
+++ src/main.c
@@ -3,12 +3,14 @@
 #include <assert.h> /* The C library macro void assert(int expression) allows diagnostic information to be written to the standard error file. */
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/pgmspace.h>
 #include "uart.h"
 #include "hmi_msg.h"
 #include "print_helper.h"
 #include "../lib/hd44780_111/hd44780.h"
 
 #define BLINK_DELAY_MS 100
+
 void blink (const char pin) /* Function for toggleing PORTA pins */
 {
     DDRA = 0x15; /* Set port A pins 0,2,4 (00010101=15h) for outputs */
@@ -21,7 +23,7 @@ static inline void init_uart_io(void)
 {
     simple_uart0_init();
     stdin = stdout = &simple_uart0_io;
-    fprintf(stdout, "%s\n", name);
+    fprintf(stdout, "%s\n", USERNAME);
     print_ascii_tbl(stdout);
     unsigned char ascii[128] = {0};
     for (unsigned char i = 0; i < sizeof(ascii); i++) {
@@ -35,10 +37,12 @@ static inline void init_errcon(void)
 {
     simple_uart1_init();
     stderr = &simple_uart1_out;
-    fprintf(stderr, "Version: %s built on: %s %s\n",
-            FW_VERSION, __DATE__, __TIME__);
-    fprintf(stderr, "avr-libc version: %s avr-gcc version: %s\n",
-            __AVR_LIBC_VERSION_STRING__, __VERSION__);
+/*    fprintf(stderr, "Version: %s built on: %s %s\n",*/
+/*            FW_VERSION, __DATE__, __TIME__);*/
+/*    fprintf(stderr, "avr-libc version: %s avr-gcc version: %s\n",*/
+/*            __AVR_LIBC_VERSION_STRING__, __VERSION__);*/
+    fprintf(stderr, VER_FW, FW_VERSION, __DATE__, __TIME__ "\n");
+    fprintf(stderr, VER_LIBC, __AVR_LIBC_VERSION_STRING__ " " VER_GCC, __VERSION__ "\n");    
 }
 
 void main (void)
@@ -47,7 +51,7 @@ void main (void)
     init_uart_io();
     lcd_init();
     lcd_home();
-    lcd_puts(name);
+    lcd_puts(USERNAME);
     int number;
 
     while (1) {
