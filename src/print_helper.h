dd9c52a3cacc6b6e9f268dd059d3be3564a41dbf
Madis <madisvorklaev@gmail.com>
2018-01-23 12:02:07 +0200

    added tallymarker_hextobin

--- src/print_helper.h
+++ src/print_helper.h
@@ -4,5 +4,6 @@
 void print_ascii_tbl (void);
 void print_for_human (const unsigned char *array, const size_t len);
 void print_bytes(const uint8_t *array, const size_t len);
+uint8_t tallymarker_hextobin(const char * str, uint8_t * bytes, size_t blen);
 
 #endif /* PRINT_HELPER */
